7066d2c1d8e234e1be6ed6434be582c68750b094
Zelialdo Sueldo da Silva Lima <71799490+zelialdo@users.noreply.github.com>
2022-11-19 23:49:07 -0300

    Update Pedidos.java

--- src/main/java/br/com/cod3r/desafio/model/entities/Pedidos.java
+++ src/main/java/br/com/cod3r/desafio/model/entities/Pedidos.java
@@ -1,7 +1,20 @@
 package br.com.cod3r.desafio.model.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Pedidos {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int numeroDoPedido;
 	
-	public Dish pedidos;
 }
